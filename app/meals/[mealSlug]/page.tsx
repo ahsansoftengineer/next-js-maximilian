f55ea5de92f7bd79edcf35004ff7f1dee23ccf31
Muhammad Ahsan <53137713+ahsansoftengineer@users.noreply.github.com>
2024-01-17 15:25:30 +0500

    IMAGE UPLOAD FORM DESIGN IS READY

--- app/meals/[mealSlug]/page.tsx
+++ app/meals/[mealSlug]/page.tsx
@@ -1,3 +1,39 @@
-export default function MealDetailsPage() {
-  return <h1>Meal Details Slug</h1>
+import Image from 'next/image';
+import classes from './page.module.css';
+import { getMeal } from '@/lib/meals';
+import { notFound } from 'next/navigation';
+export default async function MealDetailsPage({ params }: any) {
+  const meal = await getMeal(params.mealSlug);
+  if(!meal) {
+    return notFound();
+  }
+  const instructions =  meal.instructions.replaceAll('\n', '<br />')
+  console.log(meal)
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image fill src={meal.image} alt={meal.title} />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p>
+            by <a href={`mailto:${meal.creator_email}`}>
+              {meal.creator}</a>
+          </p>
+          <p className={classes.summary}>
+            {meal.summary}
+          </p>
+        </div>
+      </header>
+      <main className={classes.main}>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: instructions,
+          }}
+        ></p>
+      </main>
+    </>
+  );
 }

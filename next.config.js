7142c3f7940e3bd4359b56082c8759842e84fc85
Muhammad Ahsan <53137713+ahsansoftengineer@users.noreply.github.com>
2024-01-19 16:29:41 +0500

    AWS FILE UPLOADING WORKING

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'dqc65nwcqwvpl.cloudfront.net',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
